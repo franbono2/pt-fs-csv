1dbc7b065a0b875a5578af339ed6ad41aae505bd
franbono2 <franbono2@gmail.com>
2024-06-27 11:40:47 +0000

    Add backend with Node and TS

---
+++ backend/server.ts
@@ -0,0 +1,61 @@
+import express from 'express';
+import cors from 'cors';
+import multer from 'multer'
+import csvToJson from 'convert-csv-to-json'
+
+const app = express();
+const port = process.env.PORT ?? 3000;
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+let userData: Array<Record<string, any>> = []
+
+app.use(cors())
+app.use(express.json());
+
+// Route to handle csv file uploads
+app.post('api/files', upload.single('file'), async (req, res) => {
+    try {
+        const file = req.file;
+
+        if (!file) return res.status(400).json({ message: 'No file uploaded' });
+
+        if (file.mimetype !== 'text/csv') return res.status(400).json({ message: 'Invalid file format' });
+
+        const csvData = file.buffer.toString('utf-8')
+        console.log(csvData)
+        const jsonFromCsv = csvToJson.fieldDelimiter(',').csvStringToJson(csvData)
+
+        // Save in Memory (It could be a DB instead)
+        userData = jsonFromCsv
+        console.log(userData)
+    } catch (error) {
+        return res.status(500).json({ message: 'Error parsing the file' });
+    }
+
+    return res.status(200).json({ message: 'File uploaded successfully' });
+})
+
+// Route to filter users by query
+app.get('/api/users', async (req, res) => {
+    const q = req.query.q
+
+    if (!q) return res.status(400).json({ message: 'Please provide a search query' });
+
+    if (typeof q !== 'string') return res.status(400).json({ message: 'Please provide a valid search query' });
+
+    const search = q.toLowerCase()
+    const filteredData = userData.filter(user => {
+        return Object
+        .values(user)
+        .some(value => { value.toString().toLowerCase().includes(search) })
+    })
+
+    return res.status(200).json({ data: filteredData });
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
